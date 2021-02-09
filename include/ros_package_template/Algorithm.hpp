17525faf9442479124ee298425b5a4cc96f653b3
Qiayuan <liaoqiayuan@gmail.com>
2021-02-09 16:43:48 +0800

    Initial commit

---
+++ include/ros_package_template/Algorithm.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <memory>
+
+#include <Eigen/Core>
+
+namespace ros_package_template {
+
+/*!
+ * Class containing the algorithmic part of the package.
+ */
+class Algorithm {
+ public:
+  /*!
+   * Constructor.
+   */
+  Algorithm();
+
+  /*!
+   * Destructor.
+   */
+  virtual ~Algorithm();
+
+  /*!
+   * Add new measurement data.
+   * @param data the new data.
+   */
+  void addData(const double data);
+
+  /*!
+   * Add multiple measurements as once.
+   * @param data new data.
+   */
+  void addData(const Eigen::VectorXd &data);
+
+  /*!
+   * Get the computed average of the data.
+   * @return the average of the data.
+   */
+  double getAverage() const;
+
+ private:
+
+  //! Forward declared structure that will contain the data
+  struct Data;
+
+  //! Pointer to data (pimpl)
+  std::unique_ptr<Data> data_;
+};
+
+} /* namespace */
