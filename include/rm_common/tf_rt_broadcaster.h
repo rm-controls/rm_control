7e3887b0e3106e35232634e81423c21170fefcc8
qiayuan <liaoqiayuan@gmail.com>
2021-03-18 14:47:30 +0800

    Move robot_state_interface and tf_rt_broadcaster to rm_common

---
+++ include/rm_common/tf_rt_broadcaster.h
@@ -0,0 +1,34 @@
+//
+// Created by qiayuan on 1/3/21.
+//
+
+#ifndef RM_COMMON_TF_RT_BROADCASTER_H
+#define RM_COMMON_TF_RT_BROADCASTER_H
+#include <ros/ros.h>
+#include <tf2_msgs/TFMessage.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <realtime_tools/realtime_publisher.h>
+
+namespace robot_state_controller {
+class TfRtBroadcaster {
+ public:
+  TfRtBroadcaster() = default;
+  virtual void init(ros::NodeHandle &root_nh);
+  virtual void sendTransform(const geometry_msgs::TransformStamped &transform);
+  virtual void sendTransform(const std::vector<geometry_msgs::TransformStamped> &transforms);
+ protected:
+  ros::NodeHandle node_;
+  std::shared_ptr<realtime_tools::RealtimePublisher<tf2_msgs::TFMessage>> realtime_pub_{};
+};
+
+class StaticTfRtBroadcaster : public TfRtBroadcaster {
+ public:
+  void init(ros::NodeHandle &root_nh) override;
+  void sendTransform(const geometry_msgs::TransformStamped &transform) override;
+  void sendTransform(const std::vector<geometry_msgs::TransformStamped> &transforms) override;
+ private:
+  tf2_msgs::TFMessage net_message_{};
+};
+
+}
+#endif // RM_COMMON_TF_RT_BROADCASTER_H
