d68b0bfdf418dc8cfef7512cc580099967297c87
chenzheng <1047979458@qq.com>
2021-03-20 23:14:52 +0800

    Add unit test of lqr

--- test/LqrTest.cpp
+++ test/LqrTest.cpp
@@ -3,15 +3,17 @@
 //
 
 #include "rm_common/lqr.h"
+#include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
+TEST(Lqr, lqr
+) {
   static const size_t STATE_DIM = 6;
   static const size_t CONTROL_DIM = 2;
 
   Eigen::Matrix<double, STATE_DIM, STATE_DIM> A{}, Q{};
   Eigen::Matrix<double, STATE_DIM, CONTROL_DIM> B{};
   Eigen::Matrix<double, CONTROL_DIM, CONTROL_DIM> R{};
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> K{};
+  Eigen::Matrix<double, CONTROL_DIM, STATE_DIM> K{};
 
   A << 0, 1, 0, 0, 0, 0,
       432.8077, 0, 0, 0, 0, 0,
@@ -37,10 +39,16 @@ int main(int argc, char **argv) {
   R << 1, 0,
       0, 1;
 
+  K << -36.4112, -1.51982, -2.23607, -2.45734, -0.707107, -0.725818,
+      -36.4112, -1.51982, -2.23607, -2.45734, 0.707107, 0.725818;
+
   Lqr<double> lqr(A, B, Q, R);
   lqr.computeK();
-  K = lqr.getK();
-  std::cout << K << std::endl;
 
-  return 0;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> average = lqr.getK();
+  for (uint j = 0; j < CONTROL_DIM; ++j) {
+    for (uint i = 0; i < STATE_DIM; ++i) {
+      EXPECT_NEAR(K(j, i), average(j, i), 1e-3);
+    }
+  }
 }
