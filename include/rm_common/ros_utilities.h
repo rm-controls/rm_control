efbdb673ad751b625be62f71514b038cf9433632
chenzheng <1047979458@qq.com>
2021-04-07 12:21:29 +0800

    Delete function xmlRpcGetDouble

--- include/rm_common/ros_utilities.h
+++ include/rm_common/ros_utilities.h
@@ -30,15 +30,4 @@ inline double xmlRpcGetDouble(const XmlRpc::XmlRpcValue &value, const std::strin
   }
 }
 
-inline double xmlRpcGetDouble(const XmlRpc::XmlRpcValue &value, int field, double default_value) {
-  ROS_ASSERT((value[field].getType() == XmlRpc::XmlRpcValue::TypeDouble) ||
-      (value[field].getType() == XmlRpc::XmlRpcValue::TypeInt));
-  if (value[field].getType() == XmlRpc::XmlRpcValue::TypeInt) {
-    int tmp = value[field];
-    return (double) tmp;
-  } else {
-    return value[field];
-  }
-}
-
 #endif // RM_COMMON_ROS_UTILITIES_H
