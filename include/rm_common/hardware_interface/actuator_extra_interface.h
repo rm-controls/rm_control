610004e5ea96fb08c2652b0bad055d1f042338ad
qiayuan <liaoqiayuan@gmail.com>
2021-05-16 09:12:33 +0800

    Remove calibrated

--- include/rm_common/hardware_interface/actuator_extra_interface.h
+++ include/rm_common/hardware_interface/actuator_extra_interface.h
@@ -15,15 +15,13 @@ class ActuatorExtraHandle {
  public:
   ActuatorExtraHandle() = default;
   ActuatorExtraHandle(std::string name, bool *halted, bool *need_calibration,
-                      bool *calibrated, bool *calibration_reading, double *pos, double *offset)
+                      bool *calibration_reading, double *pos, double *offset)
       : name_(std::move(name)), halted_(halted), need_calibration_(need_calibration),
-        calibrated_(calibrated), calibration_reading_(calibration_reading), pos_(pos), offset_(offset) {
+        calibration_reading_(calibration_reading), pos_(pos), offset_(offset) {
     if (!halted)
       throw HardwareInterfaceException("Cannot create handle '" + name + "'. halted pointer is null.");
     if (!need_calibration)
       throw HardwareInterfaceException("Cannot create handle '" + name + "'. need_calibration  pointer is null.");
-    if (!calibrated)
-      throw HardwareInterfaceException("Cannot create handle '" + name + "'. calibrated pointer is null.");
     if (!calibration_reading)
       throw HardwareInterfaceException("Cannot create handle '" + name + "'. calibrated pointer is null.");
     if (!pos)
@@ -37,7 +35,6 @@ class ActuatorExtraHandle {
   std::string name_;
   bool *halted_ = {nullptr};
   bool *need_calibration_ = {nullptr};
-  bool *calibrated_ = {nullptr};
   bool *calibration_reading_ = {nullptr};
   double *pos_{};
   double *offset_{};
