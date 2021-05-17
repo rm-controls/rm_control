ed665447da706f5abc59fe735699c9dc496d14a6
astro <1015926336@qq.com>
2021-05-17 14:59:35 +0800

    Add xmlRpcGetDouble()

--- include/rm_common/ros_utilities.h
+++ include/rm_common/ros_utilities.h
@@ -29,5 +29,14 @@ inline double xmlRpcGetDouble(const XmlRpc::XmlRpcValue &value, const std::strin
     return default_value;
   }
 }
-
+inline double xmlRpcGetDouble(const XmlRpc::XmlRpcValue &value, int field, double default_value) {
+  ROS_ASSERT((value[field].getType() == XmlRpc::XmlRpcValue::TypeDouble) ||
+      (value[field].getType() == XmlRpc::XmlRpcValue::TypeInt));
+  if (value[field].getType() == XmlRpc::XmlRpcValue::TypeInt) {
+    int tmp = value[field];
+    return (double) tmp;
+  } else {
+    return value[field];
+  }
+}
 #endif // RM_COMMON_ROS_UTILITIES_H
