8deb68133d926cd37d638328afd5d40a89c64aaf
BruceLannn <1656257449@qq.com>
2021-08-31 03:07:45 +0800

    Correct rm_dbus code format.

--- rm_dbus/src/dbus_node.cpp
+++ rm_dbus/src/dbus_node.cpp
@@ -4,29 +4,36 @@
 
 #include "dbus/dbus_node.h"
 
-int main(int argc, char **argv) {
-  struct sched_param params{};
+int main(int argc, char** argv)
+{
+  struct sched_param params
+  {
+  };
   params.sched_priority = 95;
-  if (sched_setscheduler(0, SCHED_FIFO, &params) == -1) {
+  if (sched_setscheduler(0, SCHED_FIFO, &params) == -1)
+  {
     ROS_ERROR("[dbus] Set scheduler failed, RUN THIS NODE AS SUPER USER.\n");
   }
   ros::init(argc, argv, "rm_dbus");
   DBusNode dbus_node;
   ros::Rate loop_rate(60);
-  while (ros::ok()) {
+  while (ros::ok())
+  {
     dbus_node.run();
     loop_rate.sleep();
   }
   return 0;
 }
 
-DBusNode::DBusNode() {
+DBusNode::DBusNode()
+{
   dbus_pub_ = nh_.advertise<rm_msgs::DbusData>("dbus_data", 1);
   nh_.param<std::string>("serial_port", serial_port_, "/dev/usbDbus");
   dbus_.init(serial_port_.data());
 }
 
-void DBusNode::run() {
+void DBusNode::run()
+{
   dbus_.read();
   dbus_.getData(&dbus_cmd_);
   dbus_pub_.publish(dbus_cmd_);
