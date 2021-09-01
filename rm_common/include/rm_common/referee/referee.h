8fd37cd513270eb5ba4bdbb163a3088519444809
qiayuan <liaoqiayuan@gmail.com>
2021-09-01 20:11:43 +0800

    Format the code

--- rm_common/include/rm_common/referee/referee.h
+++ rm_common/include/rm_common/referee/referee.h
@@ -30,7 +30,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
- 
+
 //
 // Created by peter on 2021/5/17.
 //
