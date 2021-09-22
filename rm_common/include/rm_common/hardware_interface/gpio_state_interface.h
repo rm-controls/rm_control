076626a1fa4d8e374dd32d149a2f5646705b6d78
yezi <1536117624@qq.com>
2021-09-22 18:50:59 +0800

    Write gpio_state_interface.h(read only)

---
+++ rm_common/include/rm_common/hardware_interface/gpio_state_interface.h
@@ -0,0 +1,44 @@
+//
+// Created by yezi on 2021/9/22.
+//
+
+#ifndef SRC_RM_SOFTWARE_RM_CONTROL_RM_COMMON_INCLUDE_RM_COMMON_HARDWARE_INTERFACE_GPIO_STATE_INTERFACE_H_
+#define SRC_RM_SOFTWARE_RM_CONTROL_RM_COMMON_INCLUDE_RM_COMMON_HARDWARE_INTERFACE_GPIO_STATE_INTERFACE_H_
+
+#include <hardware_interface/internal/hardware_resource_manager.h>
+
+namespace rm_control
+{
+class GpioStateHandle
+{
+public:
+  GpioStateHandle() = default;
+  GpioStateHandle(std::string name, int* value) : name_(name), value_(value)
+  {
+    if (!value)
+      throw hardware_interface::HardwareInterfaceException("Cannot create handle '" + name +
+                                                           "'. value pointer is null.");
+  }
+  std::string getName() const
+  {
+    return name_;
+  }
+  int getValue() const
+  {
+    assert(value_);
+    return *value_;
+  }
+
+private:
+  std::string name_;
+  int* value_ = { nullptr };
+};
+
+class GpioStateInterface
+  : public hardware_interface::HardwareResourceManager<GpioStateHandle, hardware_interface::DontClaimResources>
+{
+};
+
+}  // namespace rm_control
+
+#endif  // SRC_RM_SOFTWARE_RM_CONTROL_RM_COMMON_INCLUDE_RM_COMMON_HARDWARE_INTERFACE_GPIO_STATE_INTERFACE_H_
