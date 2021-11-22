cfe54ce8251e3e95419208683602f48856e44731
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-11-22 21:32:01 +0800

    Update package.sh

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ls
 source /opt/ros/noetic/setup.bash
-sudo apt-get install python3-bloom fakeroot dh-make apt-src
+sudo apt-get install python3-bloom fakeroot dh-make devscripts
 pip install shyaml
 package_version=`curl -sL https://github.com/ros/rosdistro/raw/master/noetic/distribution.yaml | shyaml get-value repositories.rm_control.release.version`
 time_stamp=`date +%Y%m%d.%H%M%S`
