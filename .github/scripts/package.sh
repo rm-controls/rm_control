9b090aa1c71e8e1742b954165c94978877843d81
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-12-19 22:37:36 +0800

    Update package.sh

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -9,7 +9,7 @@ pip install shyaml
 
 package_version=`curl -sL https://github.com/ros/rosdistro/raw/master/noetic/distribution.yaml | shyaml get-value repositories.$ros_package_name.release.version`
 time_stamp=`date +%Y%m%d.%H%M%S`
-echo "source $ros_workspace/devel/setup.bash" | sudo tee -a /etc/profile
+#source $ros_workspace/devel/setup.bash 
 package_list=`find . -name package.xml | sed 's/package.xml//g' `
 
 for package_source in $package_list
@@ -19,7 +19,7 @@ do
   cd $package_source
   bloom-generate rosdebian --os-name ubuntu --ros-distro noetic
   debchange -v $package_version.$time_stamp -p -D -u -m 'Append timestamp when binarydeb was built.'
-  fakeroot debian/rules binary
+  fakeroot debian/rules CMAKE_PREFIX_PATH=:$ros_workspace/devel/:$CMAKE_PREFIX_PATH binary
   cd $root_directory
 done
 echo 'Package has been done.'
