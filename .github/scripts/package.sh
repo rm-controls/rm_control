356bef93ce97516e7417930dcf4e07c53853ca31
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-12-20 01:09:00 +0800

    ee

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-ros_workspace=$1
-ros_distro=$2
-ros_package_name=$3
-source /opt/ros/$ros_distro/setup.bash
+ros_package_name=$1
 
 sudo apt-get install python3-bloom fakeroot dh-make devscripts
 pip install shyaml
 
 package_version=`curl -sL https://github.com/ros/rosdistro/raw/master/noetic/distribution.yaml | shyaml get-value repositories.$ros_package_name.release.version`
 time_stamp=`date +%Y%m%d.%H%M%S`
-source $ros_workspace/devel/setup.bash
-package_list=`find . -name package.xml | sed 's/package.xml//g' `
 
 for package_source in $package_list
 do
