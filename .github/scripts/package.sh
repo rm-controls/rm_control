381275f51d83b3645f91790154521c9180d0ea41
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-12-19 20:41:39 +0800

    Update

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -1,51 +1,29 @@
 #!/bin/bash
-ls
-source /opt/ros/noetic/setup.bash
+ros_workspace=$1
+ros_distro=$2
+ros_package_name=$3
+source /opt/ros/$ros_distro/setup.bash
+
 sudo apt-get install python3-bloom fakeroot dh-make devscripts
 pip install shyaml
-package_version=`curl -sL https://github.com/ros/rosdistro/raw/master/noetic/distribution.yaml | shyaml get-value repositories.rm_control.release.version`
+
+package_version=`curl -sL https://github.com/ros/rosdistro/raw/master/noetic/distribution.yaml | shyaml get-value repositories.$ros_package_name.release.version`
 time_stamp=`date +%Y%m%d.%H%M%S`
-echo "yaml file://`pwd`/rosdep.yaml" | sudo tee /etc/ros/rosdep/sources.list.d/rm_control.list
-rosdep update
-for file in rm_msgs rm_description
+
+source $ros_workspace/devel/setup.bash
+package_list=`find . -name package.xml | sed 's/package.xml//g' `
+
+for package_source in $package_list
 do
   if test -d $file
   then
-    echo "Trying to package $file"
-    ls
-    cd $file
-    bloom-generate rosdebian --os-name ubuntu --ros-distro noetic
-    debchange -v $package_version.$time_stamp -p -D -u -m 'Append timestamp when binarydeb was built.'
-    fakeroot debian/rules binary
-    cd ..
-    sudo dpkg -i `ls -t | head -n 1`
-  fi
-done
-for file in rm_common rm_dbus rm_gazebo rm_hw
-do
-  if test -d $file
-  then
-    echo "Trying to package $file"
-    ls
-    cd $file
-    bloom-generate rosdebian --os-name ubuntu --ros-distro noetic
-    debchange -v $package_version.$time_stamp -p -D -u -m 'Append timestamp when binarydeb was built.'
-    fakeroot debian/rules binary
-    cd ..
-    sudo dpkg -i `ls -t | head -n 2 | tac`
-  fi
-done
-for file in rm_control
-do
-  if test -d $file
-  then
-    echo "Trying to package $file"
-    ls
-    cd $file
+    echo "Trying to package $package_source"
+    cd $package_source
     bloom-generate rosdebian --os-name ubuntu --ros-distro noetic
     debchange -v $package_version.$time_stamp -p -D -u -m 'Append timestamp when binarydeb was built.'
     fakeroot debian/rules binary
     cd ..
   fi
 done
-ls
+echo 'Package has been done.'
+find . -name *.deb
