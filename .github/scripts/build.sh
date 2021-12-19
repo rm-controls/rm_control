5e00cfad81572cfe20367a68c5f382bf6f4a29f0
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-12-20 01:24:01 +0800

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,10 +5,9 @@ source /opt/ros/$ros_distro/setup.bash
 
 package_list=`find $GITHUB_WORKSPACE/ -name package.xml | sed 's/package.xml//g'`
 mkdir -p $ros_workspace/src
-catkin_init_workspace $ros_workspace/src
 for package_source in $package_list
 do
-    cp -r $GITHUB_WORKSPACE/$package_source $ros_workspace/src
+    cp -r $package_source $ros_workspace/src
 done
 rosdep update
 rosdep install --from-paths $ros_workspace/src --ignore-packages-from-source --rosdistro $ros_distro -y
