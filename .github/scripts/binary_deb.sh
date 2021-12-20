f9f00e848fa18bb5e6835a5d5dcb1b539e07b64a
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-12-20 19:28:46 +0800

    Update binary_deb.sh

--- .github/scripts/binary_deb.sh
+++ .github/scripts/binary_deb.sh
@@ -31,4 +31,4 @@ do
   cd $run_directory
 done
 echo 'Package has been done.'
-find $tmp_space/src -name '*.deb' -o -name '*.ddeb'|xargs -I {} cp {} $run_directory/
+find $tmp_space -name '*.deb' -o -name '*.ddeb'|xargs -I {} cp {} $run_directory/
