034238cca8ed82d29004b91f1a618e176785439c
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-12-20 20:02:51 +0800

    ee

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -6,6 +6,7 @@ source /opt/ros/$ros_distro/setup.bash
 
 mkdir -p $ros_workspace/src/
 cp -r ./ $ros_workspace/src/
+echo 'Installing dependence..'
 rosdep update > /dev/null
 rosdep install --from-paths $ros_workspace/src --ignore-packages-from-source --rosdistro $ros_distro -y > /dev/null
 catkin_make -C $ros_workspace
