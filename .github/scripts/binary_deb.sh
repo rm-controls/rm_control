1cf64d80e936d5ced98baf7e8afe82742d60ac63
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-12-20 19:22:30 +0800

    ee

--- .github/scripts/binary_deb.sh
+++ .github/scripts/binary_deb.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-ros_workspace=$1
+ros_distro=$1
 ros_package_name=$2
+ros_workspace=$3
+tmp_space="/tmp/catkin_binary_deb"
+
+source /opt/ros/$ros_distro/setup.bash
+source $ros_workspace/devel/setup.bash
 
 echo "Install packaging tool..."
 sudo apt-get install python3-bloom fakeroot dh-make devscripts > /dev/null
 pip install shyaml > /dev/null
 
 time_stamp=`date +%Y%m%d.%H%M%S`
-package_version="`curl -sL https://github.com/ros/rosdistro/raw/master/noetic/distribution.yaml | shyaml get-value repositories.$ros_package_name.release.version`.$time_stamp"
+package_version="`curl -sL https://github.com/ros/rosdistro/raw/master/$ros_distro/distribution.yaml | shyaml get-value repositories.$ros_package_name.release.version`.$time_stamp"
 run_directory=`pwd`
-package_list=`find $ros_workspace/src -name package.xml | sed 's/package.xml//g' `
-source /opt/ros/noetic/setup.bash
-source $ros_workspace/devel/setup.bash
+
+mkdir $tmp_space
+cp -r ./ $tmp_space
+package_list=`find $tmp_space -name package.xml | sed 's/package.xml//g'`
 CM_PREFIX_PATH=`sed 's/:/;/g' <<< $CMAKE_PREFIX_PATH`
 
 for package_source in $package_list
 do
   echo "Trying to package $package_source in $package_version"
   cd $package_source
-  bloom-generate rosdebian --os-name ubuntu --ros-distro noetic
+  bloom-generate rosdebian --os-name ubuntu --ros-distro $ros_distro
   debchange -v $package_version -p -D -u -m 'Append timestamp when binarydeb was built.'
   sed -e "s|-DCMAKE_PREFIX_PATH=.*|-DCMAKE_PREFIX_PATH=\""$CM_PREFIX_PATH"\"|g" -i debian/rules
   fakeroot debian/rules binary
   cd $run_directory
 done
 echo 'Package has been done.'
-find $ros_workspace/src -name '*.deb' -o -name '*.ddeb'|xargs -I {} cp {} $run_directory/
+find $tmp_space/src -name '*.deb' -o -name '*.ddeb'|xargs -I {} cp {} $run_directory/
--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 ros_distro=$1
-ros_workspace='/tmp/catkin_ws'
+ros_workspace='/tmp/catkin_build'
+
 source /opt/ros/$ros_distro/setup.bash
 
-package_list=`find $GITHUB_WORKSPACE/ -name package.xml | sed 's/package.xml//g'`
-mkdir -p $ros_workspace/src
-for package_source in $package_list
-do
-    cp -r $package_source $ros_workspace/src
-done
+mkdir -p $ros_workspace/src/
+cp -r ./ $ros_workspace/src/
 rosdep update
 rosdep install --from-paths $ros_workspace/src --ignore-packages-from-source --rosdistro $ros_distro -y
 catkin_make -C $ros_workspace
