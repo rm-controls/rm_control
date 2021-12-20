b108e50eac43afd9e20b1ea143cad04c1edad0cd
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2021-12-20 19:37:23 +0800

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -6,7 +6,7 @@ source /opt/ros/$ros_distro/setup.bash
 
 mkdir -p $ros_workspace/src/
 cp -r ./ $ros_workspace/src/
-rosdep update
-rosdep install --from-paths $ros_workspace/src --ignore-packages-from-source --rosdistro $ros_distro -y
+rosdep update > /dev/null
+rosdep install --from-paths $ros_workspace/src --ignore-packages-from-source --rosdistro $ros_distro -y > /dev/null
 catkin_make -C $ros_workspace
 echo "::set-output name=catkin-ws-directory::$(echo $ros_workspace)"
