60ee1f4bdab621e5c7d2120e0b891b4daa550a40
yuchen <116358@qq>
2022-05-27 01:46:24 +0800

    Merge date.

--- rm_referee/include/rm_referee/robot_referee.h
+++ rm_referee/include/rm_referee/robot_referee.h
@@ -16,9 +16,83 @@ public:
 protected:
   void drawUi(const ros::Time& time) override;
 
+  void checkDbusMsg(const rm_msgs::DbusData& dbus_data)
+  {
+    // eject
+    if (dbus_data.key_ctrl && dbus_data.key_r)
+    {
+      if ((data_.referee_.referee_data_.robot_id_ == rm_common::RobotId::BLUE_HERO ||
+           data_.referee_.referee_data_.robot_id_ == rm_common::RobotId::RED_HERO))
+      {
+        gimbal_eject = true;
+      }
+    }
+    else if (dbus_data.key_w || dbus_data.key_s || dbus_data.key_a || dbus_data.key_d)
+    {
+      if ((data_.referee_.referee_data_.robot_id_ == rm_common::RobotId::BLUE_HERO ||
+           data_.referee_.referee_data_.robot_id_ == rm_common::RobotId::RED_HERO) &&
+          gimbal_eject)
+      {
+        chassis_mode = rm_msgs::ChassisCmd::FOLLOW;
+        gimbal_eject = false;
+      }
+    }
+
+    // chassis mode
+    if (std::abs(data_.dbus_data_.wheel) > 0.01)
+    {
+      chassis_mode = rm_msgs::ChassisCmd::GYRO;
+    }
+    else if (dbus_data.s_r == rm_msgs::DbusData::UP || dbus_data.s_r == rm_msgs::DbusData::DOWN ||
+             dbus_data.s_r == rm_msgs::DbusData::MID)
+    {
+      chassis_mode = rm_msgs::ChassisCmd::FOLLOW;
+    }
+    else if (dbus_data.key_ctrl && dbus_data.key_z)
+    {
+      chassis_mode = rm_msgs::ChassisCmd::FOLLOW;
+    }
+    else if (dbus_data.key_shift)
+    {
+      chassis_mode = rm_msgs::ChassisCmd::FOLLOW;
+    }
+    else if (dbus_data.key_g)
+    {
+      if (chassis_mode == rm_msgs::ChassisCmd::GYRO)
+        chassis_mode = rm_msgs::ChassisCmd::FOLLOW;
+      else
+        chassis_mode = rm_msgs::ChassisCmd::GYRO;
+    }
+    else if (dbus_data.key_e)
+    {
+      if (chassis_mode == rm_msgs::ChassisCmd::TWIST)
+        chassis_mode = rm_msgs::ChassisCmd::FOLLOW;
+      else
+        chassis_mode = rm_msgs::ChassisCmd::TWIST;
+    }
+    else if (dbus_data.key_ctrl && dbus_data.key_r)  // it will cal service
+    {
+      if (data_.referee_.referee_data_.robot_id_ == rm_common::RobotId::BLUE_HERO ||
+          data_.referee_.referee_data_.robot_id_ == rm_common::RobotId::RED_HERO)
+      {
+        chassis_mode = rm_msgs::ChassisCmd::GYRO;
+      }
+    }
+    else
+      chassis_mode = rm_msgs::ChassisCmd::FOLLOW;
+  }
+
+  void getPowerLimitStatus(double limit_power_, int referee_power_limit);
+
+  bool gimbal_eject;
+  int chassis_mode, power_limit_state;
+  // rm_common::SwitchDetectionCaller* switch_detection_srv_{};
   TimeChangeUi* time_change_ui_{};
   FlashUi* flash_ui_{};
   TriggerChangeUi* trigger_change_ui_{};
   FixedUi* fixed_ui_{};
+
+private:
+  double capacitor_threshold, extra_power, burst_power;
 };
 }  // namespace rm_referee
--- rm_referee/src/hero_referee.cpp
+++ rm_referee/src/hero_referee.cpp
@@ -16,12 +16,11 @@ void HeroReferee::drawUi(const ros::Time& time)
   RobotReferee::drawUi(time);
   if (data_.referee_.referee_data_.robot_id_ != rm_common::RobotId::BLUE_HERO &&
       data_.referee_.referee_data_.robot_id_ != rm_common::RobotId::RED_HERO)
-    trigger_change_ui_->update("target", data_.detection_status_data_.target, data_.shoot_cmd_data_.burst_flag,
+    trigger_change_ui_->update("target", data_.detection_status_data_.target, chassis_mode,
                                data_.detection_status_data_.armor_target,
                                data_.detection_status_data_.color == rm_msgs::StatusChangeRequest::RED);
   else
-    trigger_change_ui_->update("target", data_.gimbal_cmd_data_.eject_flag ? 1 : 0, data_.shoot_cmd_data_.burst_flag,
-                               data_.detection_status_data_.armor_target,
+    trigger_change_ui_->update("target", gimbal_eject ? 1 : 0, chassis_mode, data_.detection_status_data_.armor_target,
                                data_.detection_status_data_.color == rm_msgs::StatusChangeRequest::RED);
   trigger_change_ui_->update("exposure", data_.detection_status_data_.exposure, false);
   fixed_ui_->update();
