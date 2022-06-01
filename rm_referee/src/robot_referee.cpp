b9a3d558ad5628a7f5e9505875aa815f3fdfee6f
yuchen <116358@qq>
2022-06-01 13:13:43 +0800

    Change Variable name,color problem in powerlimitstate.

--- rm_referee/src/robot_referee.cpp
+++ rm_referee/src/robot_referee.cpp
@@ -18,21 +18,23 @@ RobotReferee::RobotReferee(ros::NodeHandle& nh) : RefereeBase(nh)
 void RobotReferee::getPowerLimitStatus(double limit_power_, int referee_power_limit)
 {
   if (limit_power_ == 0)
-    power_limit_state = 0;
-  else if (limit_power_ == (double)referee_power_limit)
-    power_limit_state = 2;
-  else if (limit_power_ == (double)referee_power_limit * 0.85)
-    power_limit_state = 3;
-  else if (data_.referee_.referee_data_.capacity_data.cap_power_ > capacitor_threshold)
+    power_limit_state = rm_common::PowerLimit::TEST;
+  else if (limit_power_ == (double)referee_power_limit || (data_.dbus_data_.key_ctrl && data_.dbus_data_.key_z))
+    power_limit_state = rm_common::PowerLimit::NORMAL;
+  else if (limit_power_ == (double)referee_power_limit * 0.85 || data_.dbus_data_.key_b)
+    power_limit_state = rm_common::PowerLimit::CHARGE;
+  else if (data_.referee_.referee_data_.capacity_data.cap_power_ > capacitor_threshold || data_.dbus_data_.key_shift)
   {
     if (data_.chassis_cmd_data_.mode == rm_msgs::ChassisCmd::GYRO || limit_power_ == referee_power_limit + extra_power)
-      power_limit_state = 1;
+      power_limit_state = rm_common::PowerLimit::BURST;
     else if (limit_power_ == burst_power ||
              limit_power_ == data_.referee_.referee_data_.game_robot_status_.chassis_power_limit_)
-      power_limit_state = 1;
+      power_limit_state = rm_common::PowerLimit::BURST;
   }
   else
-    ROS_WARN("Not get power limit status,ignorable if Infrequently.");
+    ROS_INFO("Not get power limit status,ignorable if Infrequently.");
+
+  ROS_INFO("Power limit state: %d", power_limit_state);
 }
 
 void RobotReferee::drawUi(const ros::Time& time)
@@ -54,7 +56,8 @@ void RobotReferee::drawUi(const ros::Time& time)
                                power_limit_state == rm_common::PowerLimit::BURST, 0,
                                power_limit_state == rm_common::PowerLimit::CHARGE);
   }
-  flash_ui_->update("spin", time, data_.chassis_cmd_data_.mode == rm_msgs::ChassisCmd::GYRO && data_.vel2d_cmd_data_.angular.z != 0.);
+  // flash_ui_->update("spin", time,
+  // data_.chassis_cmd_data_.mode == rm_msgs::ChassisCmd::GYRO && data_.vel2d_cmd_data_.angular.z != 0.);
   flash_ui_->update("armor0", time);
   flash_ui_->update("armor1", time);
   flash_ui_->update("armor2", time);
