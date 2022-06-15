fc5264e8cb0a92f0200f0f3a95db80ac8877bcbb
yezi <1536117624@qq.com>
2022-06-16 02:39:39 +0800

    Fix bug.

--- rm_common/include/rm_common/decision/command_sender.h
+++ rm_common/include/rm_common/decision/command_sender.h
@@ -278,11 +278,11 @@ public:
     ros::NodeHandle limit_nh(nh, "heat_limit");
     heat_limit_ = new HeatLimit(limit_nh, referee_data);
     if (!nh.getParam("gimbal_error_tolerance", gimbal_error_tolerance_))
-      ROS_ERROR("gimbal error limit no defined (namespace: %s)", nh.getNamespace().c_str());
+      ROS_ERROR("gimbal error tolerance no defined (namespace: %s)", nh.getNamespace().c_str());
     if (!nh.getParam("target_acceleration_tolerance", target_acceleration_tolerance_))
     {
       target_acceleration_tolerance_ = 0.;
-      ROS_INFO("target_acceleration_limit no defined(namespace: %s), set to zero.", nh.getNamespace().c_str());
+      ROS_INFO("target_acceleration_tolerance no defined(namespace: %s), set to zero.", nh.getNamespace().c_str());
     }
     double moving_average_num;
     nh.param("accleration_moving_average_num", moving_average_num, 1.);
@@ -292,7 +292,7 @@ public:
   {
     delete heat_limit_;
   }
-  void couputeTargetAcceleration()
+  void computeTargetAcceleration()
   {
     auto target_vel = track_data_.target_vel;
     double current_target_vel = sqrt(pow(target_vel.x, 2) + pow(target_vel.y, 2) + pow(target_vel.z, 2));
@@ -307,8 +307,8 @@ public:
   }
   void checkError(const rm_msgs::GimbalDesError& gimbal_des_error, const ros::Time& time)
   {
-    if (gimbal_des_error.error > gimbal_error_tolerance_ && time - gimbal_des_error.stamp < ros::Duration(0.1) &&
-        track_target_acceleration_ > target_acceleration_tolerance_)
+    if ((gimbal_des_error.error > gimbal_error_tolerance_ && time - gimbal_des_error.stamp < ros::Duration(0.1)) ||
+        (track_target_acceleration_ > target_acceleration_tolerance_))
       if (msg_.mode == rm_msgs::ShootCmd::PUSH)
         setMode(rm_msgs::ShootCmd::READY);
   }
