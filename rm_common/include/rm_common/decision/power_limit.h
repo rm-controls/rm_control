c60d08a4adf0ee220e00907160bd271c321dece8
Aung-xiao <2557160927@qq.com>
2023-04-01 11:27:31 +0800

    modify sentry power limit

--- rm_common/include/rm_common/decision/power_limit.h
+++ rm_common/include/rm_common/decision/power_limit.h
@@ -109,7 +109,7 @@ public:
   void setLimitPower(rm_msgs::ChassisCmd& chassis_cmd, bool is_gyro)
   {
     if (robot_id_ == rm_msgs::GameRobotStatus::BLUE_SENTRY || robot_id_ == rm_msgs::GameRobotStatus::RED_SENTRY)
-      chassis_cmd.power_limit = 30;
+      chassis_cmd.power_limit = 150;
     else if (robot_id_ == rm_msgs::GameRobotStatus::BLUE_ENGINEER || robot_id_ == rm_msgs::GameRobotStatus::RED_ENGINEER)
       chassis_cmd.power_limit = 400;
     else
