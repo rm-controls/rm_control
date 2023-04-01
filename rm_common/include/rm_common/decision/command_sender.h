2c64fd70279468a25cb8a74b9dcac106d199c467
1moule <1961466188@qq.com>
2023-04-01 18:05:08 +0800

    Add armor type to the judgment of checkerror.

--- rm_common/include/rm_common/decision/command_sender.h
+++ rm_common/include/rm_common/decision/command_sender.h
@@ -48,6 +48,7 @@
 #include <rm_msgs/StateCmd.h>
 #include <rm_msgs/TrackData.h>
 #include <rm_msgs/GameRobotHp.h>
+#include <rm_msgs/StatusChangeRequest.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <sensor_msgs/JointState.h>
 #include <nav_msgs/Odometry.h>
@@ -352,7 +353,7 @@ public:
   {
     if (((gimbal_des_error_.error > gimbal_error_tolerance_ && time - gimbal_des_error_.stamp < ros::Duration(0.1)) ||
          (track_data_.accel > target_acceleration_tolerance_)) ||
-        !suggest_fire_.data)
+        (!suggest_fire_.data && armor_type_ == rm_msgs::StatusChangeRequest::ARMOR_OUTPOST_BASE))
       if (msg_.mode == rm_msgs::ShootCmd::PUSH)
         setMode(rm_msgs::ShootCmd::READY);
   }
@@ -379,6 +380,10 @@ public:
     }
     return 0.;
   }
+  uint8_t setArmorType(uint8_t armor_type)
+  {
+    armor_type_ = armor_type;
+  }
   void setShootFrequency(uint8_t mode)
   {
     heat_limit_->setShootFrequency(mode);
@@ -397,6 +402,7 @@ private:
   rm_msgs::TrackData track_data_;
   rm_msgs::GimbalDesError gimbal_des_error_;
   std_msgs::Bool suggest_fire_;
+  uint8_t armor_type_{};
 };
 
 class Vel3DCommandSender : public HeaderStampCommandSenderBase<geometry_msgs::TwistStamped>
