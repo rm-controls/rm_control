93fe6290b90de48e43e3f7bb0f78aaafaa0c0ee4
yuchen <116358@qq>
2023-08-09 20:22:34 +0800

    Update switch debouncing.

--- rm_dbus/include/rm_dbus/dbus.h
+++ rm_dbus/include/rm_dbus/dbus.h
@@ -67,12 +67,12 @@ public:
   DBus() = default;
   ~DBus() = default;
   void init(const char* serial);
-  void getData(rm_msgs::DbusData* d_bus_data) const;
+  void getData(rm_msgs::DbusData& d_bus_data) const;
   void read();
 
 private:
   DBusData_t d_bus_data_{};
-  int s0_count_ = 0, s1_count_ = 0;
+  int last_s0_ = 0, last_s1_ = 0;
   int port_{};
   int16_t buff_[18]{};
   bool is_success{};
