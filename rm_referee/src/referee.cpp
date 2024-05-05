b15fdbfbb86403b1f179a6cc9a23c6d9039cef4b
Guramumei <1421887449@qq.com>
2024-05-05 21:47:58 +0800

    Update referee.

--- rm_referee/src/referee.cpp
+++ rm_referee/src/referee.cpp
@@ -468,7 +468,6 @@ int Referee::unpack(uint8_t* rx_data)
 
           client_map_send_data.target_position_x = client_map_send_data_ref.target_position_x;
           client_map_send_data.target_position_y = client_map_send_data_ref.target_position_y;
-          client_map_send_data.target_position_z = client_map_send_data_ref.target_position_z;
           client_map_send_data.command_keyboard = client_map_send_data_ref.command_keyboard;
           client_map_send_data.target_robot_ID = client_map_send_data_ref.target_robot_ID;
           client_map_send_data.cmd_source = client_map_send_data_ref.cmd_source;
@@ -479,14 +478,16 @@ int Referee::unpack(uint8_t* rx_data)
         }
         case rm_referee::SENTRY_INFO_CMD:
         {
-          rm_referee::SentryInfo sentry_info;
-          memcpy(&sentry_info, rx_data + 7, sizeof(rm_referee::SentryInfo));
+          rm_msgs::SentryInfo sentry_info;
+          memcpy(&sentry_info, rx_data + 7, sizeof(rm_msgs::SentryInfo));
+          sentry_info_pub_.publish(sentry_info);
           break;
         }
         case rm_referee::RADAR_INFO_CMD:
         {
-          rm_referee::RadarInfo radar_info;
-          memcpy(&radar_info, rx_data + 7, sizeof(rm_referee::RadarInfo));
+          rm_msgs::RadarInfo radar_info;
+          memcpy(&radar_info, rx_data + 7, sizeof(rm_msgs::RadarInfo));
+          radar_info_pub_.publish(radar_info);
           break;
         }
         case rm_referee::POWER_MANAGEMENT_SAMPLE_AND_STATUS_DATA_CMD:
