f85e1407d2f44940034bd20fe58b6ada13f511c5
liyixin135 <421159734@qq.com>
2024-07-10 15:00:35 +0800

    Add gimbal_buff_error when tracking buff.

--- rm_common/include/rm_common/decision/command_sender.h
+++ rm_common/include/rm_common/decision/command_sender.h
@@ -352,8 +352,10 @@ public:
     nh.getParam("wheel_speed_18", wheel_speed_18_);
     nh.getParam("wheel_speed_30", wheel_speed_30_);
     nh.param("extra_wheel_speed_once", extra_wheel_speed_once_, 0.);
-    if (!nh.getParam("gimbal_error_tolerance", gimbal_error_tolerance_))
-      ROS_ERROR("gimbal error tolerance no defined (namespace: %s)", nh.getNamespace().c_str());
+    if (!nh.getParam("gimbal_armor_error_tolerance", gimbal_armor_error_tolerance_))
+      ROS_ERROR("gimbal armor error tolerance no defined (namespace: %s)", nh.getNamespace().c_str());
+    if (!nh.getParam("gimbal_buff_error_tolerance", gimbal_buff_error_tolerance_))
+      ROS_ERROR("gimbal buff error tolerance no defined (namespace: %s)", nh.getNamespace().c_str());
     if (!nh.getParam("target_acceleration_tolerance", target_acceleration_tolerance_))
     {
       target_acceleration_tolerance_ = 0.;
@@ -405,6 +407,10 @@ public:
         return;
       }
     }
+    if (track_data_.id == 7)
+      gimbal_error_tolerance_ = gimbal_buff_error_tolerance_;
+    else
+      gimbal_error_tolerance_ = gimbal_armor_error_tolerance_;
     if (((gimbal_des_error_.error > gimbal_error_tolerance_ && time - gimbal_des_error_.stamp < ros::Duration(0.1)) ||
          (track_data_.accel > target_acceleration_tolerance_)) ||
         (!suggest_fire_.data && armor_type_ == rm_msgs::StatusChangeRequest::ARMOR_OUTPOST_BASE))
@@ -491,6 +497,8 @@ private:
   double wheel_speed_10_{}, wheel_speed_15_{}, wheel_speed_16_{}, wheel_speed_18_{}, wheel_speed_30_{},
       wheel_speed_des_{};
   double gimbal_error_tolerance_{};
+  double gimbal_armor_error_tolerance_{};
+  double gimbal_buff_error_tolerance_{};
   double target_acceleration_tolerance_{};
   double extra_wheel_speed_once_{};
   double total_extra_wheel_speed_{};
