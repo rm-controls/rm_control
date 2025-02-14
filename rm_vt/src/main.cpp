5fc36551a66ed1617d9e0d1562a21d435752a0c0
CH <3631676002@qq.com>
2025-02-14 11:32:06 +0800

    Add rm_vt for video transmission link.

---
+++ rm_vt/src/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by chen on 24-11-23.
+//
+#include "rm_vt/video_tran.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "rm_vt");
+  ros::NodeHandle nh("~");
+  rm_vt::VideoTran video_tran(nh);
+  ros::Rate loop_rate(100);
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    video_tran.read();
+    loop_rate.sleep();
+  }
+}
