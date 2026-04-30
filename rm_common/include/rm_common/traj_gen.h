8a587f49017434f7d336318e6c264bae08a08a1c
ZiDXie <shengxie47@gmail.com>
2026-04-30 15:25:04 +0000

    Add traj.

--- rm_common/include/rm_common/traj_gen.h
+++ rm_common/include/rm_common/traj_gen.h
@@ -199,3 +199,41 @@ public:
     }
   }
 };
+
+template <typename T>
+class NonlinearTrackingDifferentiator
+{
+public:
+  NonlinearTrackingDifferentiator(T r, T h) : r_(r), h_(h)
+  {
+  }
+  void update(T v, T v_dot, T af)
+  {
+    if ((v > 3. && x1_ < -3.) || (v < -3 && x1_ > 3))
+    {
+      x1_ = v;
+      x2_ = 0;
+    }
+    else
+    {
+      T y = x1_ - v + h_ * x2_;
+      T a0 = sqrt(h_ * h_ * r_ * r_ + 8 * r_ * fabs(y));
+      T a = x2_ + af * (a0 - h_ * r_) * (y > 0 ? 1 : -1);
+      T u = fabs(a) > h_ * r_ ? -r_ * (a > 0 ? 1 : -1) : -r_ * a / (h_ * r_);
+      x1_ = x1_ + h_ * (x2_ + v_dot);
+      x2_ = x2_ + h_ * u;
+    }
+  }
+  T getX1() const
+  {
+    return x1_;
+  }
+  T getX2() const
+  {
+    return x2_;
+  }
+
+private:
+  T r_{}, h_{};
+  T x1_{}, x2_{};
+};
